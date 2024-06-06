7e897feb080e4f1e72fbb1fcb701bb03644a0702
ShinkarevaAnna <klio-muza@rambler.ru>
2024-06-06 20:13:12 +0300

    lesson1

---
+++ src/Itmo/lessons1/Main.java
@@ -0,0 +1,21 @@
+package Itmo.lessons1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Task1 task1 = new Task1();
+        Task2 task2 = new Task2();
+        Task3 task3 = new Task3();
+        Task4 task4 = new Task4();
+        Task5 task5 = new Task5();
+        Task6 task6 = new Task6();
+
+        task1.wordsOut();
+        System.out.println(task2.expressionResult1());
+        System.out.println(task2.expressionResult2());
+        task3.count();
+        task4.multiplication();
+        task5.outPutNumbers();
+        task6.condition();
+    }
+}
---
+++ src/Itmo/lessons1/Task2.java
@@ -0,0 +1,13 @@
+package Itmo.lessons1;
+
+public class Task2 {
+    public double expressionResult1() {
+        double result1 = ( 46 + 10 ) * ( 10 / 3 );
+        return result1;
+
+    }
+    public int expressionResult2() {
+        int result2 = ( 29 ) * ( 4 ) * (- 15 );
+        return result2;
+    }
+}
