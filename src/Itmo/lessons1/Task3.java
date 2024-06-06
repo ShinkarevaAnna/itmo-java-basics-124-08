0f4ef516ac8dd9d6d2bc081397c80afcc259c0d2
a.shinkareva.lor <a.shinkareva.lor@gmail.com>
2024-06-06 14:48:20 +0300

    Add lesson1

---
+++ src/Itmo/lessons1/Task3.java
@@ -0,0 +1,9 @@
+package Itmo.lessons1;
+
+public class Task3 {
+    public void count(){
+        int number = 10500;
+        int result = (number/10)/10;
+        System.out.println(result);
+    }
+}
