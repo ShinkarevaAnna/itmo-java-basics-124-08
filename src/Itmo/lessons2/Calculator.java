d28b73e21818733328ae9898f944281aa1b62efc
ShinkarevaAnna <klio-muza@rambler.ru>
2024-06-07 21:14:22 +0300

    add lesson2

---
+++ src/Itmo/lessons2/Calculator.java
@@ -0,0 +1,40 @@
+package Itmo.lessons2;
+//нужно ли объявлять переменные и потом в методе присваивать передаваемые параметры через this?
+public class Calculator {
+    public double sum(double a, double b){
+        return a+b;
+    }
+    public int sum(int c, int d){
+        return c+d;
+    }
+    public long sum(long e, long f){
+        return e+f;
+    }
+    public double division(double a, double b){
+        return a/b;
+    }
+    public int division(int c, int d){
+        return c/d;
+    }
+    public long division(long e, long f){
+        return e/f;
+    }
+    public double multiplication(double a, double b){
+        return a*b;
+    }
+    public int multiplication(int c, int d){
+        return c*d;
+    }
+    public long multiplication(long e, long f){
+        return e*f;
+    }
+    public double subtraction(double a, double b){
+        return a-b;
+    }
+    public int subtraction(int c, int d){
+        return c-d;
+    }
+    public long subtraction(long e, long f){
+        return e-f;
+    }
+}
