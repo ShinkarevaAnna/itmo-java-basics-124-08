aaf2c2180ec07faad5a22461c7647d2ab96a5300
ShinkarevaAnna <klio-muza@rambler.ru>
2024-06-11 20:05:30 +0300

    change lesson4

--- src/Itmo/lessons4/Part1/Share.java
+++ src/Itmo/lessons4/Part1/Share.java
@@ -1,46 +1,30 @@
 package Itmo.lessons4.Part1;
 
 public class Share {
-
-    public static void printArray(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > 0) {
-                System.out.print(arr[i] + ", ");
-            }
+    public static void divisibilityCheck(){
+        int i3 =0;
+        int i5 = 0;
+        int i35 = 0;
+        int x = 0;
+        System.out.print( "Делится на 3: ");
+        while (x  < 100 && i3 + 3 <=100) {
+            System.out.print((i3 = i3 +3) + ",");
+            x++;
         }
         System.out.println();
-    }
-    public static void divisibilityCheck(int[] arr3, int[] arr5, int[] arr35 ){
-        int j = 0;
-        for (int i = 1; i < 100; i++) {
-            if (i % 3 == 0) {
-                arr3[j] = i;
-                j++;
-            }
+        System.out.print( "Делится на 5: ");
+        while (x  < 100 && i5 + 5 <=100) {
+            System.out.print((i5 = i5 + 5) + ",");
+            x++;
         }
-        for (int i = 1; i < 100; i++) {
-            if (i % 5 == 0) {
-                arr5[j] = i;
-                j++;
-            }
+        System.out.println();
+        System.out.print( "Делится на 3 и на 5: ");
+        while (x<100 && i35 + 15 <= 100) {
+            System.out.print((i35 = i35 + 15) + ",");
+            x++;
         }
-        for (int i = 1; i < 100; i++) {
-            if ((i % 3 == 0) && (i % 5 == 0)) {
-                arr35[j] = i;
-                j++;
-            }
-        }
-    }
+  }
     public static void main(String[] args) {
-        int[] arr3 = new int[80];
-        int[] arr5 = new int[80];
-        int[] arr35 = new int[80];
-        divisibilityCheck(arr3, arr5,arr35);
-        System.out.println("Делится на 3: ");
-        printArray(arr3);
-        System.out.println("Делится на 5: ");
-        printArray(arr5);
-        System.out.println("Делится на 3 и на 5: ");
-        printArray(arr35);
+        divisibilityCheck();
     }
 }
